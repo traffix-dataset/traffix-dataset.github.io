b6ce6a74b356b871c95a261f3174f70d02afe384
traffix-dataset <>
2023-12-05 15:39:17 +0100

    add license

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,17 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-
-$(document).ready(function() {
+$(document).ready(function () {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
     }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
+    // Initialize all div with carousel class
+    bulmaCarousel.attach('.carousel', options);
     bulmaSlider.attach();
-
 })
